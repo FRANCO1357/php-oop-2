28c60604ea138642153874f5b2adf179ca59f585
FRANCO1357 <fmelani13@gmail.com>
2022-09-13 16:12:48 +0200

    var_dump $prodotto

--- models/index.php
+++ models/index.php
@@ -1,5 +1,9 @@
 <?php
 
+include_once __DIR__ . '/models/products/FoodProduct.php';
 
+$prodotto = new FoodProduct('Crocchette', 'cibo per gatti', 4.99, ['gatti'], ['manzo', 'pollo', 'tacchino'], "30-10-2022");
+
+var_dump($prodotto);
 
 ?>
--- models/products/FoodProduct.php
+++ models/products/FoodProduct.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__  '/Product.php'
+require_once __DIR__ . '/Product.php'
 
 class FoodProduct extends Product{
     private $ingredients;
